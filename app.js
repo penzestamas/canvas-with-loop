2be97f7e21dc3f35f7e3891111425d9fb22d3e3d
pthomas2001 <71702126+pthomas2001@users.noreply.github.com>
2021-03-10 13:32:30 +0100

    js

--- app.js
+++ app.js
@@ -1,2 +1,11 @@
 var canvas = document.getElementById('myCanvas');
 var context = canvas.getContext('2d');
+
+var size = 45;
+var distance = 20;
+context.fillStyle = 'rgba(255,165,0,.5)';
+
+for(var i = 0; i < 10; i++){
+  context.fillRect(distance, distance, size, size);
+  distance += size / 2;
+}
